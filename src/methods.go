9c0f1a54b905fcb9837e495666ace934adcf1d5f
bushimichi <bushimichi@gmail.com>
2017-02-26 06:48:54 -0500

    update

--- src/methods.go
+++ src/methods.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-  "fmt"
-  "math"
+	"fmt"
+	"math"
 )
 
-type Vertex struct{
-  X, Y float64
+type Vertex struct {
+	X, Y float64
 }
 
 func (self Vertex) Abs() float64 {
-  return math.Sqrt(self.X * self.X + self.Y * self.Y)
+	return math.Sqrt(self.X*self.X + self.Y*self.Y)
 }
 
 func Abs(self Vertex) float64 {
-  return math.Sqrt(self.X * self.X + self.Y * self.Y)
+	return math.Sqrt(self.X*self.X + self.Y*self.Y)
 }
 
 type MyFloat float64
+
 func (f MyFloat) Abs() float64 {
-  if f < 0 {
-    return float64(-f)
-  }
-  return float64(f)
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
 }
 
+func main() {
+	v := Vertex{3, 4}
+	fmt.Println(v.Abs())
+	fmt.Println(Abs(v))
 
-func main(){
-  v := Vertex{ 3, 4 }
-  fmt.Println(v.Abs())
-  fmt.Println(Abs(v))
-
-  f := MyFloat(-math.Sqrt2)
-  fmt.Println(f.Abs())
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println(f.Abs())
 }
